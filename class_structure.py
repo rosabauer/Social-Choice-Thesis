baee0f8e4da406b8d50891cdbd3491cd99eb80f0
Rosa Bauer <rosa.bauer@icloud.com>
2024-06-01 19:44:14 +0200

    new branch for different dissenter method

--- class_structure.py
+++ class_structure.py
@@ -179,7 +179,7 @@ class Crowd: # Some sort of dynamic process tracker / protocol initally, now a c
             return 'B'
         else:
             print("Tie! Adjust number of agents using set_no_of_agents.")
-            return None
+            return 'X'
 
     # TBD: might be able to use this method for lazy dissenters as well and only ajust the update_top function of the agents    
     def dissenters_keen(self, profile):
@@ -187,12 +187,17 @@ class Crowd: # Some sort of dynamic process tracker / protocol initally, now a c
         winner = self.get_winner(profile) # This may be dangerous to do? Maybe control this variable outside the method.
 
         # Initialize boolean array to indicate whether agent dissents or not
-        dissent = [0] * len(profile)
+        dissent = [None] * len(profile)
 
         i = 0
         while i < len(profile):
             if profile[i] != winner: # This works for keen dissenters, since 'X' is not 'A', so this loop would activate for indifference 
-                dissent[i] = 1
+                # Handling the special case of an indifferent agent
+                if profile[i] == 'X':
+                    dissent[i] = 'B' if winner == 'A' else 'A' 
+                    # We dont need to handle case 'X' here, since then the bigger encasing if clause would not be true since X = X.
+                else:
+                    dissent[i] = profile[i]
             i += 1
         
         return dissent
@@ -234,8 +239,8 @@ class Crowd: # Some sort of dynamic process tracker / protocol initally, now a c
         print(profile) # ___Printer___
         majority = self.get_winner(profile)
         minority = 'A' if majority == 'B' else 'B'
-        revealed_in_round_A = [0] * A_EVIDENCE
-        revealed_in_round_B = [0] * B_EVIDENCE
+        revealed_in_round_A = [0] * self.A_EVIDENCE
+        revealed_in_round_B = [0] * self.B_EVIDENCE
 
         # Initialize dissenters
         dissenters = self.dissenters_keen(profile)
@@ -244,7 +249,7 @@ class Crowd: # Some sort of dynamic process tracker / protocol initally, now a c
         print('First majority vote:', majority) # ____Printer____
         print()
 
-        while 1 in dissenters:
+        while dissenters != [None] * len(self.agents):
 
             # Start of a new round
             round += 1
@@ -253,26 +258,25 @@ class Crowd: # Some sort of dynamic process tracker / protocol initally, now a c
             new_evidence_revealed = False
             
             # For all dissenters, add their evidence for the minority alternative into the revealed evidence sets
-            minority_evidence = f'es_{minority}'
-            print('The minority evidence: ', minority_evidence)
         
-            for i, is_dissenter in enumerate(dissenters):
-                if is_dissenter == 1:
+            for i, is_dissenter_letter in enumerate(dissenters):
+                if is_dissenter_letter != None:
                     # Get the set of evidence for the minority option from each dissenter
-                    agent_evidence = getattr(self.agents[i], minority_evidence)
-                    print(f'Agent {i} s agent_evidence for the minority {minority_evidence}: ', agent_evidence) # ___Printer___
+                    dissenter_evidence = f'es_{is_dissenter_letter}'
+                    dissenter_evidence = getattr(self.agents[i], dissenter_evidence)
+                    print(f'Agent {i} s dissenter_evidence for the minority {is_dissenter_letter}: ', dissenter_evidence) # ___Printer___
                     
-                    # Update the revealed_in_round sets to take on the minority_evidence (only one if-clause will be activated)
-                    if minority == 'A':
-                        for j in range(len(agent_evidence)):
-                            if agent_evidence[j] == 1 and revealed_in_round_A[j] != 1:
+                    # Update the revealed_in_round sets to take on the the revealed evidence (only one if-clause will be activated)
+                    if is_dissenter_letter == 'A':
+                        for j in range(len(dissenter_evidence)):
+                            if dissenter_evidence[j] == 1 and revealed_in_round_A[j] != 1:
                                 revealed_in_round_A[j] = 1
                                 new_evidence_revealed = True # Sets revealed marker to true
 
                                 print(f'Revealing new evidence {j} for minority option {minority} from agent {i}') # ___Printer___
-                    if minority == 'B':
-                        for k in range(len(agent_evidence)):
-                            if agent_evidence[k] == 1 and revealed_in_round_B[k] != 1:
+                    if is_dissenter_letter == 'B':
+                        for k in range(len(dissenter_evidence)):
+                            if dissenter_evidence[k] == 1 and revealed_in_round_B[k] != 1:
                                 revealed_in_round_B[k] = 1
                                 new_evidence_revealed = True # Sets revealed marker to true
 
@@ -326,8 +330,9 @@ class Crowd: # Some sort of dynamic process tracker / protocol initally, now a c
         print('Generating plot for deliberation end result...')
         self.plot_agent_evidence(self.agents)
 
+        print(f'_______________WINNER {self.get_winner(self.profile)}.______________')
         print(f'Termination at round {round}: No more dissenters.' if ret_reason == None else f'Termination at round {round}: {ret_reason}')
-        return majority
+        return self.get_winner(self.profile)
         
 
 # Running the simulations
